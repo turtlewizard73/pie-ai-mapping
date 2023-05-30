944b1f6de4d3725851d0c91b9c03eff4436dd4ba
Máté <56221639+turtlewizard73@users.noreply.github.com>
2023-05-30 12:03:04 +0200

    Put pose onto map (#3)
    * add yolo as submodule
    
    * add darknet yolo subm
    
    * add bringup launch for gazebo and rviz
    
    * darknet launch and config files
    
    * add world with tables and chairs
    
    * not fixing table world
    
    * add worlds
    
    * fix bringup launch file
    
    * add tensorflow and weight convert
    
    * add common object detection
    
    * update gitignore to ignore weights
    
    * add threading
    
    * multithreading input output fix
    
    * remove darknet subm
    
    * rename custom msg
    
    * remove unused launch files
    
    * update readme
    
    * add tf listener
    
    * add drawing marker on robot
    
    * add parameters to correct depth
    
    * thats all folks
    
    * add dependencies
    
    * write readme

--- README.md
+++ README.md
@@ -2,6 +2,84 @@
 
 depends on project: https://github.com/turtlewizard73/pie-mapping-and-navigation.git
 
+## Setup and install
+1. **Clone repository and update submodules**
+    ```
+    mkdir -p workspace/src
+    cd workspace/src
+    git clone https://github.com/turtlewizard73/pie-mapping-and-navigation.git
+    git clone https://github.com/turtlewizard73/pie-ai-mapping.git
+    ```
+
+2. **Checkout on development and update submodules**
+    ```
+    cd src/pie-mapping-and-navigation
+    git fetch
+    git checkout development
+    git pull
+    git submodule update --init --recursive
+
+    cd src/pie-ai-mapping
+    git fetch
+    git checkout development
+    git pull
+    git submodule update --init --recursive
+    ```
+
+3. **Build gazebo actors plugin**
+    ```
+    cd src/pie-mapping-and-navigation/gazebo_actor_collisions_plugin
+    mkdir build
+    cd build
+    cmake ..
+    make
+    export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$(pwd)
+    ```
+
+4. **Install dependencies and build the packages**
+    ```
+    cd workspace/src/pie-mapping-and-navigation
+    sudo xargs -a apt-dependencies.txt apt install -y
+
+    cd workspace/src/pie-ai-mapping
+    sudo xargs -a apt-dependencies.txt apt install -y
+    pip3 install -r pip-dependencies.txt
+
+    cd workspace
+    catkin_make
+    source devel/setup.bash
+    ```
+
+5. **Download and load weights**
+    ```
+    cd workspace/src/pie-ai-mapping/pie_detection/weigths
+    wget https://pjreddie.com/media/files/yolov3.weights
+
+    python3 pie-ai-mapping/pie_detection/scripts/convert_weights.py
+    ```
+
+## How to use
+1. **start the simulation** (gazebo & rviz)
+    ```
+    roslaunch pie_ai_bringup test.launch
+    ```
+
+2. **moving the robot**
+    ```
+    rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+    ```
+
+3. **Start the object and people detection**
+    ```
+    rosrun pie_detection image_detector.py
+    ```
+
+4. **Start displaying markers**
+    ```
+    rosrun pie_detection pose_enplace.py
+    ```
+
+## stuff to sort out
 cd yolo
 pip install -r requirements.txt
 catkin_make --make-args run_tests
@@ -14,4 +92,23 @@ https://www.tensorflow.org/install/pip#windows-wsl2_1
 https://docs.nvidia.com/cuda/wsl-user-guide/index.html
 
 yolo weigths: https://pjreddie.com/darknet/yolo/
+
 people detection:
+    yolos scriptek:
+https://machinelearningspace.com/yolov3-tensorflow-2-part-1/
+https://machinelearningspace.com/yolov3-tensorflow-2-part-2/
+https://machinelearningspace.com/yolov3-tensorflow-2-part-3/
+https://machinelearningspace.com/yolov3-tensorflow-2-part-4/
+https://github.com/RahmadSadli/Deep-Learning/tree/master/YOLOv3_TF2
+
+    cuda telepítés:
+        https://towardsdatascience.com/how-to-finally-install-tensorflow-gpu-on-windows-10-63527910f255
+        https://towardsdatascience.com/how-to-finally-install-tensorflow-gpu-on-wsl2-7be59e278f92
+        https://www.tensorflow.org/install/source#gpu
+
+    image segmentation:
+        https://medium.com/analytics-vidhya/image-segmentation-with-yolov3-and-grabcut-59a0abaafa3e
+        https://github.com/Cuda-Chen/fish-yolo-grabcut/blob/master/utils/GrabCut.py
+        https://pyimagesearch.com/2021/01/19/image-masking-with-opencv/
+
+install: bottleneck
---
+++ pie_detection/scripts/pose_enplace.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+# Python modules
+import cv2
+import numpy as np
+import tf
+import math
+from tf2_ros.buffer import Buffer
+
+# Ros modules
+import rospy
+from nav_msgs.msg import OccupancyGrid
+from visualization_msgs.msg import Marker
+
+# Custom modules
+from pie_detection.msg import CamPose, CamPoses
+from cvthread import ProcessThread, cvThread, BufferQueue
+
+# https://github.com/awesomebytes/occupancy_grid_python
+class PoseEnplacer():
+    def __init__(self) -> None:
+        self.horizontal_fov = 1.047
+        self.pixel_width = 640
+        self.object_pose_sub = rospy.Subscriber(
+            '/detection/camposes',
+            CamPoses,
+            self.object_pose_callback
+        )
+
+        self.listener = None
+
+        self.queueSize = 1
+        self.map_queue = BufferQueue(self.queueSize)
+        self.object_pose_queue = BufferQueue(self.queueSize)
+        self.robot_pose_queue = BufferQueue(self.queueSize)
+        self.marker = Marker()
+
+        self.marker_pub = rospy.Publisher(
+            'visualization_marker',
+            Marker,
+            queue_size=1
+        )
+
+    def object_pose_callback(self, msg):
+        print('cooking')
+        i = 0
+        for pose in msg.poses:
+            print(pose)
+            name = pose.name
+            depth = pose.mean_depth * 5
+            x_min = pose.x_min*2
+            x_max = pose.x_max*2
+
+            valid_width = 2*depth*math.atan(self.horizontal_fov/2)
+
+            valid_x_min = x_min / self.pixel_width * valid_width
+            valid_x_max = x_max / self.pixel_width * valid_width
+            center = (valid_x_min + valid_x_max) / 2
+
+            marker = Marker()
+            marker.header.frame_id = 'base_link'
+            marker.header.stamp = rospy.Time.now()
+            marker.ns = name
+            marker.id = i
+            marker.type = Marker.CUBE
+            marker.action = Marker.ADD
+
+            marker.pose.position.x = depth
+            marker.pose.position.y = -(-valid_width/2 + center)
+            marker.pose.position.z = 0
+            marker.pose.orientation.x = 0
+            marker.pose.orientation.y = 0
+            marker.pose.orientation.z = 0
+            marker.pose.orientation.w = 0
+
+            marker.scale.x = 0.45
+            marker.scale.y = valid_x_max - valid_x_min
+            marker.scale.z = 0.1
+            marker.color.a = 1.0 # Don't forget to set the alpha!
+            marker.color.r = 1.0 if name == 'person' else 0.0
+            marker.color.g = 1.0 if (name == 'chair') or (name == 'sofa') else 0.0
+            marker.color.b = 0.0
+
+            i = i+1
+            self.marker_pub.publish(marker)
+
+    # def robot_pose_on_map(self):
+    #     trans, rot = 0, 0
+    #     try:
+    #         (trans,rot) = listener.lookupTransform('/map', '/base_link', rospy.Time(0))
+    #     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+    #         print(e)
+    #     return trans, rot
+    
+
+def main():
+    print("OpenCV version: %s" % cv2.__version__)
+    rospy.init_node('pose_enplace_node')
+
+    pose_enplacer = PoseEnplacer()
+    tf_buffer = Buffer()
+    listener = tf.TransformListener(tf_buffer)
+
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    rospy.init_node('turtle_tf_listener')
+
+    # listener = tf.TransformListener()
+    pose_enplacer = PoseEnplacer()
+    rospy.spin()
+
+    # rate = rospy.Rate(10.0)
+    # while not rospy.is_shutdown():
+    #     trans, rot = pose_enplacer.robot_pose_on_map()
+    #     try:
+    #         (trans,rot) = listener.lookupTransform('/base_link', '/map', rospy.Time(0))
+    #     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+    #         print(e)
+    #         continue
+    #     print(trans, rot)
+    #     pose_enplacer.object_pose_callback(trans, rot)
+
+    #     rate.sleep()
